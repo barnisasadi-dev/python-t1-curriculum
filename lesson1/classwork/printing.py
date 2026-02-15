21d055c93817c90968a5b9b2172e340bc7eb454a
barnisasadi-dev <barnis.asadi@gmail.com>
2026-02-14 18:12:26 -0800

    Update printing.py

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,12 @@
+print ("hello world!")
+print ("my name is Sana!")
+print (' i like dogs')
+print ('hi") # wrong code
+x = 5 # integer = whole number without decimal
+print (x)
+favorite_food = "pizza"
+
+print(favorite_food)
+print (x, favorite_food) # you can print multiple thing by using a comma
+y = 3.25
+print (y, x, favorite_food)
